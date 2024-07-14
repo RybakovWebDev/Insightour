8e10d6585ac5e4d05dc88f42a4718b0253d16ffa
Andrey Rybakov <rybakovwebdev@gmail.com>
2024-07-14 21:07:05 +0400

    Only load header when logo is ready

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,41 @@
 "use client";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { LazyMotion, m } from "framer-motion";
 
 import styles from "./Header.module.css";
 
 import HeaderNav from "../HeaderNav";
 
 import { useRefsContext } from "@/contexts/RefsContext";
-
 import { logoImage } from "@/constants";
 
+const loadFeatures = () => import("../../features").then((res) => res.default);
+
 function Header() {
+  const [logoLoaded, setLogoLoaded] = useState(false);
   const { headerRef } = useRefsContext();
 
+  const handleLoaded = () => {
+    setLogoLoaded(true);
+  };
+
   return (
-    <header ref={headerRef} className={styles.wrapper}>
-      <Link href={"/"}>
-        <Image className={styles.image} src={logoImage} height={80} alt='Insightour logo' sizes={"100px"} />
-      </Link>
-      <HeaderNav />
-    </header>
+    <LazyMotion features={loadFeatures}>
+      <m.header
+        ref={headerRef}
+        className={styles.wrapper}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: logoLoaded ? 1 : 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Link href={"/"}>
+          <Image src={logoImage} height={80} alt='Insightour logo' sizes={"100px"} onLoad={handleLoaded} />
+        </Link>
+        <HeaderNav />
+      </m.header>
+    </LazyMotion>
   );
 }
 
